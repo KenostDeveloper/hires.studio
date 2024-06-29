543dccc10931404724ab71bba7de111ab0e65d27
Kenost <Kenostru@mail.ru>
2024-06-29 21:48:12 +0500

    upd

--- server_image/app.js
+++ server_image/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
  
 // app.use("/static", express.static("public"));
-app.use("/static", express.static("./public"));
+app.use("/image", express.static("./public"));
  
 app.use("/", function(_, response){
      
