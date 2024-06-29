9a385a7b0c878a57c633cb1f45d2661c4699aefc
Kenost <Kenostru@mail.ru>
2024-06-29 22:10:11 +0500

    upd

--- server_image/app.js
+++ server_image/app.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
  
 // app.use("/static", express.static("public"));
-app.use("/", express.static("./public"));
-
+app.use("/static", express.static("./public"));
+ 
+app.use("/", function(_, response){
+     
+    response.send("<h1>Главная страница</h1>");
+});
  
 app.listen(2000, () => {console.log("Listening on port 2000")});
